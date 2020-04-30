1fdf2a25426648fb6fbe7a4d0047cf519d24fcdb
Thomas Tighe <ttighe@reisystems.com>
2020-04-30 13:58:55 -0400

    508 changes for color contrast and click

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,17 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function topFunctionKeyboard(event) {
+    if (event.keyCode === 13) {
+        topFunction();
+    }
+}
 // jQuery( document ).ready(function($) );
-jQuery( document ).ready(function($) {
-    $(function() {
-        $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-        $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+jQuery(document).ready(function ($) {
+    $(function () {
+        $("#tabs").tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
+        $("#tabs li").removeClass("ui-corner-top").addClass("ui-corner-left");
     })
 
 });
