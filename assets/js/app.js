953d67aa76a53e6f1dfbbe1a2b39f6523894ef09
Shivankar Ojha <shivankar.ojha@C02XCBC4JG5J.local>
2020-05-19 15:02:45 -0400

    accordion fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,9 +10,6 @@ jQuery(document).ready(function ($) {
     $(function () {
         $("#tabs").tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
         $("#tabs li").removeClass("ui-corner-top").addClass("ui-corner-left");
-        $( "#accordion" ).accordion({
-            collapsible: true
-        });
     })
 
 });
