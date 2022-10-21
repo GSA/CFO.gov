5f9d8b355feed302b5354ae520b8b61c71033412
HarikaMunipalle <harika.munipalle@gsa.gov>
2022-10-21 10:20:07 -0400

    ..

--- assets/js/facets.js
+++ assets/js/facets.js
@@ -68,19 +68,14 @@ $.getJSON(window.federalist.path.baseurl + '/search.json', function(res) {
         $elem.trigger('change');
       }
     });
-     jQuery(document).ready(function ($) {
-          jQuery('#job-career-competency-select-all').click(function (e) {
-              e.preventDefault();
-              if ($('.job-career-competency-select').css('display') == 'none') {
-                  $('#job-career-competency-select-all').html("De-Select All");
-                  $('#job-career-competency-select-all').attr("aria-label", "De-Select All");
-              } else {
-                  $('#job-career-competency-select-all').html("Select All");
-                  $('#job-career-competency-select-all').attr("aria-label", "Select All");
-              }
-              jQuery('.job-career-competency-select').slideToggle();
-          });
-      });
+      if ($('.job-career-competency-select').css('display') == 'none') {
+          $('#job-career-competency-select-all').html("De-Select All");
+          $('#job-career-competency-select-all').attr("aria-label", "De-Select All");
+      } else {
+          $('#job-career-competency-select-all').html("Select All");
+          $('#job-career-competency-select-all').attr("aria-label", "Select All");
+      }
+      jQuery('.job-career-competency-select').slideToggle();
   });
 
   // create an array of everything of both disabled and active.
