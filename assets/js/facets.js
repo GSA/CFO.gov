0520525593bc429e4abf7b60ec1b1cb0863065e0
HarikaMunipalle <harika.munipalle@gsa.gov>
2022-10-21 22:26:02 -0400

    ..

--- assets/js/facets.js
+++ assets/js/facets.js
@@ -144,7 +144,7 @@ $.getJSON(window.federalist.path.baseurl + '/search.json', function(res) {
 
         $("#" + eventId).on('change', function () {
           console.log("Select All Competency Group");
-            var labelId = "#competency-group-label-" + eventId.toLower();
+            var labelId = "#competency-group-label-" + eventId;
           console.log(labelId);
           if ($(labelId).text() == 'Select All') {
               $(labelId).html("<strong>De-Select All</strong>");
