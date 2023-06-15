e48e0fec0f02619a24a75a79ec2e1a3d540c9cf9
HarikaMunipalle <harika.munipalle@gsa.gov>
2023-06-15 14:16:06 -0400

    added new excel

--- assets/buildCards.js
+++ assets/buildCards.js
@@ -4,7 +4,7 @@ const parse = require('csv-parse').parse;
 function buildCards() {
     let cards = {};
 
-    fs.createReadStream('assets/csv/FEDS Competency Model Sample Data Golf.csv')
+    fs.createReadStream('assets/csv/FCModelEcho.csv')
         .pipe(parse({ columns: true }))
         .on('data', function (row) {
             const parts = row[Object.keys(row)[0]].split(' '),
