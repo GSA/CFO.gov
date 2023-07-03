3e59416934db9cc17fff44f87d3e6c130ce98a3c
Illia Stankevych <illia.stankevych@gsa.gov>
2023-07-03 13:05:02 -0400

    ECASOGP-9832:  Implement custom Url for template pages FRPC pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,16 @@ function topFunction() {
 // jQuery( document ).ready(function($) );
 jQuery(document).ready(function ($) {
     $(function () {
-        $("#tabs").tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
+        $("#tabs").tabs(
+            {
+                activate: function (event, ui) {
+                    let path = ui.newPanel.data('path');
+                    if (path !== undefined) {
+                        window.history.pushState({}, null, window.location.origin + path + '#' + ui.newPanel.attr('id'));
+                    }
+                }
+            }
+        ).addClass("ui-tabs-vertical ui-helper-clearfix");
         $("#tabs li").removeClass("ui-corner-top").addClass("ui-corner-left");
     })
 
