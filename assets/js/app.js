fac405aa8fdcd02aab258c9d1ebe6ed1def6c547
Luna Wang <lunawang@lunas-mbp.lan>
2023-07-03 15:17:33 -0400

    fixed back url

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,9 +11,8 @@ jQuery(document).ready(function ($) {
         $("#tabs").tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
         $("#tabs li").removeClass("ui-corner-top").addClass("ui-corner-left");
     })
-
 });
 
-window.onhashchange = function() {
+$(window).on('hashchange', function(e){
     location.reload();
-}
+   });
