d7ab84cd816b9ee26e2151a395eee871668a8ce7
Luna Wang <lunawang@lunas-mbp.lan>
2023-07-03 21:55:13 -0400

    updated eventlisttener function

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,17 +13,8 @@ jQuery(document).ready(function ($) {
     })
 });
 
-(function(history){
-    var pushState = history.pushState;
-    history.pushState = function(state) {
-        if (typeof(history.onpushstate) == "function") {
-            history.onpushstate({state: state});
-        }
-        // Call the original pushState method
-        return pushState.apply(history, arguments);
-    };
-})(window.history);
-
-window.onpopstate = history.onpushstate = function(event) {
-    location.reload();
-};
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+});
