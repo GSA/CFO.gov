1d425645a42e4a03c9ebc3f390fbdab75b64f4ea
Luna Wang <lunawang@lunas-mbp.lan>
2023-12-04 16:48:43 -0500

    updated wfm

--- assets/js/facets/facets-result/facets-result.js
+++ assets/js/facets/facets-result/facets-result.js
@@ -79,7 +79,7 @@
                 '<button class="show-more usa-button usa-button--outline border-0 bg-white" data-more-text="Show More" data-less-text="Show Less" data-index="'+ i +'" aria-label="Show More card number '+ i +'">Show More</button>\n' +
                 '</div>';
         }
-        coursesDiv.innerHTML = '<p class="font-body-md card-display card-text-color"><strong>COURSE LISTINGS FOR PURPOSE:</strong></p>' + coursesMarkup;
+        coursesDiv.innerHTML = '<p class="font-body-md card-display card-text-color"><strong>VENDOR COURSE LISTINGS</strong></p>' + coursesMarkup;
         outerDiv3.append(coursesDiv);
 
         const selectButtonWrapper = document.createElement('div');
